8338d77f011775d46043373d0d30b854256a2ee3
Laurent Doguin <ldoguin@nuxeo.com>
2011-12-14 12:50:50 +0100

    add default tags for the wizard

--- media/js/tag_wizard.js
+++ media/js/tag_wizard.js
@@ -15,11 +15,16 @@
  */
 
 var products = [
-    {name: 'Nuxeo DM', id: 'document-management'},
-    {name: 'Nuxeo EP', id: 'ecm-platform'},
-    {name: 'Nuxeo DAM', id: 'digital-asset-management'},
-    {name: 'Nuxeo CMF', id: 'case-management'},
+    {name: 'Document Management', id: 'document-management'},
+    {name: 'Nuxeo Platform', id: 'ecm-platform'},
+    {name: 'Digital Asset Management', id: 'digital-asset-management'},
+    {name: 'Digital Asset Management', id: 'content-management-platform'},
+    {name: 'Case Management', id: 'case-management'},
+    {name: 'Social Collaboration', id: 'social-collaboration'},
+    {name: 'Mobile Content Management', id: 'nuxeo-mobile'},
+    {name: 'Nuxeo Cloud', id: 'nuxeo-cloud'},
     {name: 'Nuxeo Studio', id: 'nuxeo-studio'},
+    {name: 'Nuxeo IDE', id: 'nuxeo-ide'},
     {name: 'Nuxeo Maketplace', id: 'nuxeo-marketplace'},
     {name: 'Nuxeo Connect', id: 'nuxeo-connect'},
 ]
